356c0a069fc10706173746a806483f34ebfcd332
Sfeng666 <siyuanfeng.bioinfo@gmail.com >
2023-11-20 15:11:07 -0600

    test scripts extracting fixed sites

---
+++ fixed_sites_panel/code/extract_fixed_sites_across_lines.py
@@ -0,0 +1,97 @@
+# This script extract sites that have the same allele fixed across all extracted parental lines from above vcf.gz files
+import os
+from optparse import OptionParser
+
+# each vcf.gz has the same genomic sites of a parental line
+# each row is a site, and each column is a field of the site
+# the vcf.gz files have a header like this: CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	GENOTYPE
+# more detailed explanation for the headers can be found here: http://www.johnpool.net/vcf_header_info.txt
+# sites to be extracted should meet the following criteria:
+# 1) the site is homozygous (same genotype) in any given parental line, which means the first segment in column 10 is either '0/0' (homozygous at the reference allele 'REF') or '1/1' (homozygous at the alternative allele 'ALT'); and
+# 2) the genotype of the site is the same across all parental lines (i.e., vcf.gz files); and
+# 3) the genotype of the site is not missing in any parental lines, which means column 6 must not be '.'; and
+# 4) the sites must meet the filtering criterion, which means column 7 must not be 'LowQual'.
+# the output file is a standard VCF 4.2 format without headers that begins with '###'
+
+### help & usage ### 
+usage = "usage: %prog [options] args"
+description = "extract sites that have the same allele fixed across all extracted parental lines"
+version = '%prog 11.20.2023'
+parser = OptionParser(usage=usage,version=version, description = description)
+parser.add_option("--path_parental_vcfs",
+                    action="store",
+                    dest="path_parental_vcfs",
+                    help="path to the directory that include site information of all parental lines (.vcf.gz)",
+                    metavar = 'PATH')
+parser.add_option("--path_out_vcf",
+                    action="store",
+                    dest="path_out_vcf",
+                    help="path to the output VCF file that include sites fixed across all parental lines (.vcf)",
+                    metavar = 'PATH')
+                                                                                                    
+(options,args) = parser.parse_args()
+#############################
+
+### parameters ###
+path_parental_vcfs = options.path_parental_vcfs
+path_out_vcf = options.path_out_vcf
+
+# define a function to get all file paths under a directory
+def get_file_paths(directory_path):
+    file_paths = []
+    for root, _, files in os.walk(directory_path):
+        for file in files:
+            file_paths.append(os.path.join(root, file))
+    return file_paths
+
+# define a function to compare genotype site-by-site across all parental lines
+def compare_site_genotype(file_paths, path_out_vcf):
+    file_handles = [open(file, 'r') for file in file_paths]
+
+    with open(path_out_vcf, 'a') as out:
+        num_line = 0
+        while True:
+            lines = [file.readline().strip() for file in file_handles]  # Read one line from each file
+            lines_split = [line.split('\t') for line in lines]  # Split columns by tab
+            if any(lines):  # Check if at least one file still has lines
+                if all(lines):  # Check if all files have non-empty lines
+                    if num_line == 0:
+                        out.write(lines[0] + '\n')    # write the same input header to the output file
+
+                    qual = [line[5] for line in lines_split]  # Extract QUAL column
+                    if '.' in qual: # filter sites that have missing genotype in any parental line
+                        continue
+
+                    filter = [line[6] for line in lines_split]  # Extract FILTER column
+                    if 'LowQual' in filter: # filter sites that have LowQual in any parental line
+                        continue
+
+                    genotypes = [line[9].split(':')[0].split('/') for line in lines_split]  # Extract GENOTYPE column
+                    if list(genotype for genotype in genotypes if len(set(genotype)) == 2) != []: # filter sites that are heterozygous in any parental line
+                        continue
+
+                    alleles = [line[4:6] for line in lines_split]  # Extract REF and ALT column (refernce and alternative alleles)
+                    fixed_alleles = [alleles[i][set(genotypes[i])[0]] for i in range(len(genotypes))]   # generate a list of alleles that are fixed across all parental lines, using genotype number as the index on alleles
+                    if len(set(fixed_alleles)) == 1: # extract sites that are fixed on the same allele across all parental lines
+                        # keep writing the site information to the output file in VCF 4.2 format during the loop, instead of write them all at once after the loop
+                        # this is to save memory, because the output file can be very large
+                        # the output file is a standard VCF 4.2 format with header
+                        # the header is the same as the header of the input files
+                        # the output file is saved as 'fixed_sites_panel/data/sites_parental_lines.txt'
+                        out.write('\t'.join(lines_split[0][:3] + ['.', set(fixed_alleles)[0], '.', '.', '.', '.', '.']) + '\n')
+                else:
+                    print("Number of lines in files differs.")
+                    break
+            else:
+                break
+            num_line += 1
+
+    # Close all file handles
+    for file_handle in file_handles:
+        file_handle.close()
+
+# get all file paths of .vcf.gz files under the directory
+file_paths = get_file_paths(path_parental_vcfs)
+
+# extract fixed sites across all parental lines with the function defined above
+compare_site_genotype(file_paths, path_out_vcf)
--- fixed_sites_panel/code/prep_sites_vcf_parental_lines.sh
+++ fixed_sites_panel/code/prep_sites_vcf_parental_lines.sh
@@ -2,7 +2,14 @@
 
 
 path_vcf_tar=/raid10/backups/octavius/nexus1.1/POOL_sites_vcfs.tar  # path to the tar file containing vcf files containing genotype information for all sites of all Drosophila nexus 1.1 strains
+path_vcf_parental=/raid10/boqin/data/dmel_cold_tolerance/SNP/parental_lines_vcfs  # path to the folder containing the vcf files for parental lines
 path_filereport=fixed_sites_panel/data/filereport_read_run_PRJNA720479_include_samplename.txt   # path to the file containing the sample names 
+path_log=fixed_sites_panel/data/log_prep_sites_vcf_parental_lines.txt  # path to the log file
+path_script_fixed_sites=fixed_sites_panel/code/extract_fixed_sites_from_vcf.py  # path to the python script that extracts fixed sites from vcf files
+path_vcf_fixed=fixed_sites_panel/data/fixed_sites_panel.vcf  # path to vcf file containing the fixed sites for parental lines (maintained as .vcf only for using 'bcftools consensus' later, only useful information is the first four column)
+path_ref_genome=/raid10/Tiago/PigmCages/scripts/alignment_software/dmel_ref/DmelRef.fasta # path to the reference genome sequence (release 5)
+path_ref_genome_mutated=fixed_sites_panel/data/DmelRef_mutated_at_fixed_sites.fasta  # path to the reference genome sequence (release 5) mutated with the fixed alterinative alleles
+
 
 # 1. from the tar file, extract the vcf files for the parental lines, whose file names include the name of each parental line
 # the names of parental lines are included in sample names in the last column (column name is sample_alias) of $path_filereport, which looks like the following snippet:
@@ -10,5 +17,19 @@ path_filereport=fixed_sites_panel/data/filereport_read_run_PRJNA720479_include_s
 # PRJNA720479	SAMN18651930	SRX10548219	SRR14180007	7227	Drosophila melanogaster	ftp.sra.ebi.ac.uk/vol1/fastq/SRR141/007/SRR14180007/SRR14180007_1.fastq.gz;ftp.sra.ebi.ac.uk/vol1/fastq/SRR141/007/SRR14180007/SRR14180007_2.fastq.gz	ftp.sra.ebi.ac.uk/vol1/srr/SRR141/007/SRR14180007	SD54N_parent
 # one example is to extract 'SD54N' as the name of parental line, from the sample name 'SD54N_parent'
 # the following command extracts the vcf files for the parental lines, and save them in the folder 'fixed_sites_panel/data/parental_lines_vcfs'
-mkdir -p fixed_sites_panel/data/parental_lines_vcfs
-cat $path_filereport | awk -F'\t' 'NR>1{print $NF}' | awk -F'_' '{print $1}' | sort | uniq | while read line; do tar -xvf $path_vcf_tar -C fixed_sites_panel/data/parental_lines_vcfs --wildcards --no-anchored "*${line}*.vcf"; done
+mkdir -p $path_vcf_parental
+echo "Decompression starts at: $(date)" > $path_log
+cat $path_filereport | awk -F'\t' 'NR>1{print $NF}' | awk -F'_' '{print $1}' | sort | uniq | while read line; do tar -xvf $path_vcf_tar -C $path_vcf_parental --strip-components=1 --wildcards --no-anchored "*${line}_sites.vcf.gz"; done >> $path_log 2>&1 &
+echo "Decompression ends at: $(date)" >> $path_log
+
+# 2. extract sites that have the same genotype fixed across all extracted parental lines from above vcf.gz files
+echo "Extracting sites starts at: $(date)" >> $path_log
+python $path_script_fixed_sites $path_vcf_parental $path_vcf_fixed >> $path_log 2>&1 &
+echo "Extracting sites ends at: $(date)" >> $path_log
+
+# 3. mutate the reference genome sequence (release 5) with the fixed sites, if fixed at alternative alleles
+# use 'bcftools concensus'
+echo "Mutating reference genome starts at: $(date)" >> $path_log
+bcftools consensus -f $path_ref_genome -o $path_ref_genome_mutated $path_vcf_fixed >> $path_log 2>&1 &
+echo "Mutating reference genome ends at: $(date)" >> $path_log
+
